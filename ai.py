a3aec2a02988124afe4ca9ccb58c6a92ef262674
Sidharth Ramesh <tornadoalert@gmail.com>
2023-05-13 17:05:05 +0530

    added prompts

---
+++ ai.py
@@ -0,0 +1,27 @@
+from langchain.chat_models import ChatOpenAI
+from langchain import ConversationChain
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.prompts.prompt import PromptTemplate
+from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory
+# from socketcallback import SocketIOCallback
+from langchain.chains import LLMChain
+import os
+
+llm = ChatOpenAI(model='gpt-4', temperature=0.5, streaming=True)
+
+doctor_helper_template = PromptTemplate(input_variables=["transcript"],
+                                        template="""
+You need to convert the following into Hindi
+Transcript: {transcript}
+""")
+
+patient_instructions_template = PromptTemplate(input_variables=["summary"],
+                                               template="""
+{summary}
+""")
+
+doctor_helper = LLMChain(llm=llm, prompt=doctor_helper_template)
+patient_instructor = LLMChain(llm=llm, prompt=patient_instructions_template)
+
+result = doctor_helper.run({"transcript": "I have fever since 3 days"}, callbacks=[StreamingStdOutCallbackHandler()])
+# print(result)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from serve import serve
 if __name__ == "__main__":
     print("Running transcribe thread")
 
-
     print("Running server thread")
+    serve()
